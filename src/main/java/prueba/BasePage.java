90640ff544932a315fd4a8363f2a1f9ec51c7e7e
speranzamiriam <speranzamiriam2@gmail.com>
2023-03-01 20:58:25 -0400

    codigo talle1

---
+++ src/main/java/prueba/BasePage.java
@@ -0,0 +1,16 @@
+package prueba;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePage {
+    public final WebDriverWait wait;
+    protected WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+    }
+}
---
+++ src/main/java/prueba/LoginPage.java
@@ -0,0 +1,56 @@
+package prueba;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
+
+public class LoginPage extends BasePage {
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void open() {
+        driver.get("https://opencart.abstracta.us/");
+        driver.manage().window().maximize();
+    }
+
+    public void selectOptions(String value) {
+        driver.findElement(By.xpath("//a[contains(@title,'My Account')]")).click();
+        WebElement content = driver.findElement(By.id("top-links"));
+        List<WebElement> list = content.findElements(By.tagName("a"));
+        for (WebElement element : list) {
+            if (element.getText().equals(value)) {
+                JavascriptExecutor js = (JavascriptExecutor) driver;
+                js.executeScript("arguments[0].click();", element);
+                break;
+            }
+        }
+    }
+
+    public void enterCredentials(String user, String pass) {
+        driver.findElement(By.id("input-email")).sendKeys(user);
+        driver.findElement(By.id("input-password")).sendKeys(pass);
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+    }
+
+    public boolean isLoadedTextBox() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input-email"))).isDisplayed();
+    }
+
+    public String getSuccessValue() {
+        WebElement value = driver.findElement(By.xpath("//h2[text()='My Account']"));
+        return value.getText();
+    }
+
+    public String getErrorValue() {
+        WebElement error = driver.findElement(By.xpath("//div[contains(@class,'alert')]"));
+       return error.getText();
+    }
+
+
+}
